a9272a3d356f0fa65047a3384dc99a632ca5b57b
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2021-08-17 21:55:33 -0300

    Add pastas frontend1 e imperativa

---
+++ Imperativa/aula4/script.js
@@ -0,0 +1,13 @@
+// Comentário uma linha.
+
+/*
+    Comentário de múltiplas linhas.
+    Utilizada principalmente em documentação de códido.
+*/
+
+var saudacao = 'Olá!';
+var nome = prompt('Qual o seu nome?');
+
+alert(saudacao + ' ' + nome);
+
+
